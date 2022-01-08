ca2cfc0dacef17c28131bb55af7509daa1747e4c
YashMaxy <yash.vadi@yahoo.com>
2022-01-08 23:33:36 +0530

    add station names to history

--- controller/booking.controller.js
+++ controller/booking.controller.js
@@ -1,4 +1,5 @@
 const Booking = require("./../db/models/booking.model");
+const Station = require("./../db/models/station.model");
 
 async function addBooking(req,res){
     let {station_id, slot, b_date, charger_type, mobile_no} = req.body;
@@ -40,10 +41,21 @@ async function getBookingHistory(req,res){
     try{
         const booking = new Booking();
         booking.getBookingHistory(mobile_no)
-            .then(result => {
+            .then(async result => {
+                let obj = result;
+                let station = new Station();
+
+                for(let i=0; i<obj.rows.length; i++){
+                    await station.getStationName(obj.rows[i].station_id)
+                        .then(result1 => {
+                            obj.rows[i]['station_name'] = result1.rows[0].name;
+                        });
+                }
+
+
                 let cur_date = new Date();
-                let pastBooking = result.rows.filter(i=> new Date(i.b_date).getDate() < cur_date.getDate());
-                let booking =  result.rows.filter(i=> new Date(i.b_date).getDate() >= cur_date.getDate());
+                let pastBooking = obj.rows.filter(i=> new Date(i.b_date).getDate() < cur_date.getDate());
+                let booking =  obj.rows.filter(i=> new Date(i.b_date).getDate() >= cur_date.getDate());
                 res.status(200).json({pastBooking, booking});
             });
         
